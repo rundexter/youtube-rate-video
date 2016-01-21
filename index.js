38eabf9622f27be80ac7e85a05024c85edfa8516
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-21 19:29:51 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var _ = require('lodash'),
     google = require('googleapis'),
-    util = require('./util.js');
-
-var service = google.youtube('v3');
+    util = require('./util.js'),
+    service = google.youtube('v3');
 
 var pickInputs = {
         id: 'id',
@@ -10,33 +9,6 @@ var pickInputs = {
     };
 
 module.exports = {
-
-    /**
-     * Get auth data.
-     *
-     * @param step
-     * @param dexter
-     * @returns {*}
-     */
-    authOptions: function (step, dexter) {
-        var OAuth2 = google.auth.OAuth2,
-            oauth2Client = new OAuth2();
-
-        if(!dexter.environment('google_access_token')) {
-
-            this.fail('A [google_access_token] environment variable is required for this module');
-
-            return false;
-        } else {
-
-            oauth2Client.setCredentials({
-                access_token: dexter.environment('google_access_token')
-            });
-
-            return oauth2Client;
-        }
-    },
-
     /**
      * The main entry point for the Dexter module
      *
@@ -44,19 +16,16 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var auth = this.authOptions(step, dexter);
-
-        if (!auth)
-            return;
+        var OAuth2 = google.auth.OAuth2,
+            oauth2Client = new OAuth2(),
+            credentials = dexter.provider('google').credentials();
         // set credential
-        google.options({ auth: auth });
-
-        service.videos.rate(util.pickStringInputs(step, pickInputs), function (error, data) {
-
-            if (error)
-                this.fail(error);
-            else
-                this.complete({success: true});
+        oauth2Client.setCredentials({
+            access_token: _.get(credentials, 'access_token')
+        });
+        google.options({ auth: oauth2Client });
+        service.videos.rate(util.pickInputs(step, pickInputs), function (error) {
+            error? this.fail(error) : this.complete({success: true});
         }.bind(this));
     }
 };
